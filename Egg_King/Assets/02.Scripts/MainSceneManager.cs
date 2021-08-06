16e40d7d6fce15ea078e6110cd2bdee5220363d9
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-08-06 11:04:13 +0900

    임시 카메라 무빙

--- Egg_King/Assets/02.Scripts/MainSceneManager.cs
+++ Egg_King/Assets/02.Scripts/MainSceneManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainSceneManager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class MainSceneManager : MonoBehaviour
     [SerializeField] GameObject crackedEggPrefab;
     [SerializeField] Transform crackedEggPool;
     [SerializeField] EggCounter eggCounter;
+
+    [SerializeField] RectTransform mainPanelOfCvsEggCrack;
+    [SerializeField] RectTransform mainPanelOfCvsEggMachine;
 
     [SerializeField] Button zoneMakeEgg;
 
@@ -32,6 +36,29 @@ public class MainSceneManager : MonoBehaviour
     private void Start()
     {
         zoneMakeEgg.onClick.AddListener(() => MakeEgg(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            ChangeCvsEggCrackToEggMachine(0.5f);
+    }
+#endif
+
+    public void ChangeCvsEggCrackToEggMachine(float changeTime)
+    {
+        Sequence seq = DOTween.Sequence();
+
+        mainPanelOfCvsEggCrack.anchoredPosition = new Vector2(0, 0);
+        mainPanelOfCvsEggMachine.anchoredPosition = new Vector2(0, -1920);
+        mainPanelOfCvsEggCrack.gameObject.SetActive(true);
+        mainPanelOfCvsEggMachine.gameObject.SetActive(true);
+
+        eggCounter.gameObject.SetActive(false);
+        seq.Append(mainPanelOfCvsEggCrack.DOAnchorPosY(1920, changeTime).SetEase(Ease.Linear));
+        seq.Join(mainPanelOfCvsEggMachine.DOAnchorPosY(0, changeTime).SetEase(Ease.Linear));
+        seq.OnComplete(() => mainPanelOfCvsEggCrack.gameObject.SetActive(false));
     }
 
     public void MakeEgg(Vector2 makePos)
