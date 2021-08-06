0406eae64669228d61c8674d8c34d3c37c0c8e99
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-08-06 11:44:46 +0900

    계란 기계 -> 계란 깨기 화면전환 및 예외처리

--- Egg_King/Assets/02.Scripts/MainSceneManager.cs
+++ Egg_King/Assets/02.Scripts/MainSceneManager.cs
@@ -6,6 +6,13 @@ using DG.Tweening;
 
 public class MainSceneManager : MonoBehaviour
 {
+    public enum UIState
+    {
+        EggCrack,
+        EggMachine,
+        Recipe
+    }
+
     public static MainSceneManager Instance = null;
 
     [SerializeField] GameObject eggPrefab;
@@ -18,6 +25,10 @@ public class MainSceneManager : MonoBehaviour
     [SerializeField] RectTransform mainPanelOfCvsEggMachine;
 
     [SerializeField] Button zoneMakeEgg;
+
+    UIState uiState = UIState.EggCrack;
+
+    private bool isChangingUI;
 
     short crackedEggCount = 0;
     public short CrackedEggCount { get { return crackedEggCount; } set { crackedEggCount = value; eggCounter.UpdateText(crackedEggCount); } }
@@ -35,34 +46,86 @@ public class MainSceneManager : MonoBehaviour
 
     private void Start()
     {
-        zoneMakeEgg.onClick.AddListener(() => MakeEgg(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        zoneMakeEgg.onClick.AddListener(() =>MakeEgg(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
     }
 
 #if UNITY_EDITOR
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            ChangeCvsEggCrackToEggMachine(0.5f);
+        {
+            switch (uiState)
+            {
+                case UIState.EggCrack:
+                    ChangeCvsEggCrackToEggMachine(0.5f);
+                    break;
+                case UIState.EggMachine:
+                    ChangeCvsEggMachineToEggCrack(0.5f);
+                    break;
+                case UIState.Recipe:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 #endif
 
     public void ChangeCvsEggCrackToEggMachine(float changeTime)
     {
+        if (isChangingUI) return;
+
         Sequence seq = DOTween.Sequence();
 
         mainPanelOfCvsEggCrack.anchoredPosition = new Vector2(0, 0);
         mainPanelOfCvsEggMachine.anchoredPosition = new Vector2(0, -1920);
+
         mainPanelOfCvsEggCrack.gameObject.SetActive(true);
         mainPanelOfCvsEggMachine.gameObject.SetActive(true);
 
         eggCounter.gameObject.SetActive(false);
+        crackedEggPool.gameObject.SetActive(false);
+        isChangingUI = true;
+
         seq.Append(mainPanelOfCvsEggCrack.DOAnchorPosY(1920, changeTime).SetEase(Ease.Linear));
         seq.Join(mainPanelOfCvsEggMachine.DOAnchorPosY(0, changeTime).SetEase(Ease.Linear));
-        seq.OnComplete(() => mainPanelOfCvsEggCrack.gameObject.SetActive(false));
+        seq.OnComplete(() => {
+            mainPanelOfCvsEggCrack.gameObject.SetActive(false);
+            uiState = UIState.EggMachine;
+            isChangingUI = false;
+        });
+    }
+
+    public void ChangeCvsEggMachineToEggCrack(float changeTime)
+    {
+        if (isChangingUI) return;
+
+        Sequence seq = DOTween.Sequence();
+
+        mainPanelOfCvsEggCrack.anchoredPosition = new Vector2(0, 1920);
+        mainPanelOfCvsEggMachine.anchoredPosition = new Vector2(0, 0);
+
+        mainPanelOfCvsEggCrack.gameObject.SetActive(true);
+        mainPanelOfCvsEggMachine.gameObject.SetActive(true);
+        isChangingUI = true;
+
+        seq.Append(mainPanelOfCvsEggCrack.DOAnchorPosY(0, changeTime).SetEase(Ease.Linear));
+        seq.Join(mainPanelOfCvsEggMachine.DOAnchorPosY(-1920, changeTime).SetEase(Ease.Linear));
+        seq.OnComplete(() => {
+            crackedEggPool.gameObject.SetActive(true);
+            eggCounter.gameObject.SetActive(true);
+            mainPanelOfCvsEggMachine.gameObject.SetActive(false);
+            uiState = UIState.EggCrack;
+            isChangingUI = false;
+        });
+
     }
 
     public void MakeEgg(Vector2 makePos)
     {
+        if (isChangingUI)
+            return;
+
         GameObject egg = PoolManager.GetItem<EggScript>().gameObject;
         egg.transform.position = makePos;
     }
