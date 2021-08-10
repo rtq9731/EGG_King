e075496d5215c841fb7ab8ce548643ff98f50e00
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-08-10 21:34:12 +0900

    클래스 이름 변경

---
+++ Egg_King/Assets/02.Scripts/VO/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+
+
+
+    int crackedEggLimit = 30;
+    int eggMachineLimit = 30;
+}
--- Egg_King/Assets/02.Scripts/VO/RecipeData.cs
+++ Egg_King/Assets/02.Scripts/VO/RecipeData.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Data
+public class RecipeData : MonoBehaviour
 {
-    int crackedEggLimit = 30;
-    int eggMachineLimit = 30;
+    
 }
