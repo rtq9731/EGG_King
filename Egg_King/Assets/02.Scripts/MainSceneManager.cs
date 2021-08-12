5ceef341f1430e0135465839456eb986af1328f3
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-08-12 16:09:22 +0900

    데이터 저장 및 프로젝트 폐기 결정

--- Egg_King/Assets/02.Scripts/MainSceneManager.cs
+++ Egg_King/Assets/02.Scripts/MainSceneManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Text;
+using System.IO;
 
 public class MainSceneManager : MonoBehaviour
 {
@@ -28,28 +30,85 @@ public class MainSceneManager : MonoBehaviour
 
     UIState uiState = UIState.EggCrack;
 
+    PlayerData data = new PlayerData();
+
     private bool isChangingUI;
 
     short crackedEggCount = 0;
     public short CrackedEggCount { get { return crackedEggCount; } set { crackedEggCount = value; eggCounter.UpdateText(crackedEggCount); } }
 
+    string FilePath;
+
     private void Awake()
     {
+
+#if UNITY_EDITOR
+        Debug.Log(Application.persistentDataPath);
+#endif
+
+        // 데이터 저장 경로 지정
+        FilePath = Application.persistentDataPath + "/data.sav";
+
+        // 싱글턴을 위한 Instance 생성
         Instance = this;
+
+        // 풀 생성
         PoolManager.CreatePool<EggScript>(eggPrefab, eggPool);
         PoolManager.CreatePool<BrokenEgg>(crackedEggPrefab, crackedEggPool);
     }
     private void OnDestroy()
     {
+        // 지워질 땐 싱글턴에서 삭제
         Instance = null;
     }
 
     private void Start()
     {
+        // 깨진 계란을 생성해주는 버튼 초기화
         zoneMakeEgg.onClick.AddListener(() =>MakeEgg(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
     }
 
+    /// <summary>
+    /// 현재 MainSceneManager에 담긴 PlayerData 저장 
+    /// </summary>
+    public void SaveGame()
+    {
+        string jsonString = JsonUtility.ToJson(data);
+        FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate);
+        StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+        sw.Write(jsonString);
+
+        sw.Close();
+        fs.Close();
+    }
+
+    /// <summary>
+    /// 현재 저장 경로에있는 PlayerData 불러오기
+    /// </summary>
+    public void LoadGame()
+    {
+        if(!File.Exists(FilePath))
+        {
+            SaveGame();
+            return;
+        }
+
+        FileStream fs = new FileStream(FilePath, FileMode.Open);
+        StreamReader sr = new StreamReader(fs);
+
 #if UNITY_EDITOR
+        Debug.Log(sr.ReadToEnd());
+#endif
+
+        JsonUtility.FromJson<PlayerData>(sr.ReadToEnd());
+
+        sr.Close();
+        fs.Close();
+    }
+
+
+#if UNITY_EDITOR // 전부 에디터에서 테스트용 코드
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -68,9 +127,23 @@ public class MainSceneManager : MonoBehaviour
                     break;
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SaveGame();
+            Debug.Log(JsonUtility.ToJson(data));
+        }
+        else if( Input.GetKeyDown(KeyCode.L))
+        {
+            LoadGame();
+        }
     }
 #endif
 
+    /// <summary>
+    /// EggCrackCvs에서 EggMachineCvs로 넘겨주는 함수
+    /// </summary>
+    /// <param name="changeTime"></param>
     public void ChangeCvsEggCrackToEggMachine(float changeTime)
     {
         if (isChangingUI) return;
@@ -99,6 +172,10 @@ public class MainSceneManager : MonoBehaviour
         });
     }
 
+    /// <summary>
+    /// EggMachineCvs에서 EggCrackCvs로 넘겨주는 함수
+    /// </summary>
+    /// <param name="changeTime"></param>
     public void ChangeCvsEggMachineToEggCrack(float changeTime)
     {
         if (isChangingUI) return;
@@ -126,6 +203,10 @@ public class MainSceneManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// makePos의 위치에 깨질 계란을 만들어줍니다.
+    /// </summary>
+    /// <param name="makePos"></param>
     public void MakeEgg(Vector2 makePos)
     {
         if (isChangingUI)
@@ -135,10 +216,22 @@ public class MainSceneManager : MonoBehaviour
         egg.transform.position = makePos;
     }
 
+    /// <summary>
+    /// makePos의 위치에 이미 깨진 계란을 만들어줍니다.
+    /// </summary>
+    /// <param name="makePos"></param>
     public void MakeCrackedEgg(Vector2 makePos)
     {
         GameObject egg = PoolManager.GetItem<BrokenEgg>().gameObject;
         egg.transform.position = makePos;
     }
 
+    /// <summary>
+    /// 지금 CrackedEgg( -> 담겨있는 깨진 계란 )을 EggMachine으로 옮겨줍니다.
+    /// </summary>
+    /// <param name="CrackedEggCount"></param>
+    public void CrackedEggToEggMachine(int CrackedEggCount)
+    {
+
+    }
 }
--- Egg_King/Assets/02.Scripts/VO/PlayerData.cs
+++ Egg_King/Assets/02.Scripts/VO/PlayerData.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerData
 {
+    public int crackedEggLimit = 30;
+    public int eggMachineLimit = 30;
 
+    public PlayerData(int crackedEggLimit, int eggMachineLimit)
+    {
+        this.crackedEggLimit = crackedEggLimit;
+        this.eggMachineLimit = eggMachineLimit; 
+    }
 
-
-    int crackedEggLimit = 30;
-    int eggMachineLimit = 30;
+    public PlayerData()
+    {
+        this.crackedEggLimit = 30;
+        this.eggMachineLimit = 30;
+    }
 }
